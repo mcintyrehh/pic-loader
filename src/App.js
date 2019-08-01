fa1544128963a3bee6083bfdbdc3b613407ece58
Henry <mcintyrehh@gmail.com>
2019-07-31 22:06:47 -0400

    app up, starting to incorperate some formatting structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout, Row, Col } from 'antd';
 import './App.css';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header>
+          <Row>
+            <Col span={8} offset={8}>
+              <div className="logo">
+                <span role="img" aria-label="thumbs up emoji">👍</span>
+                Thumbpic
+                <span role="img" aria-label="thumbs up emoji">📸</span>
+              </div>
+            </Col>
+          </Row>
+        </Header>
+        <Content>
+          <div className="container">
+            {/* <Row> <Link></Link><Button>TV</Button></Row> */}
+            
+          </div>
+        </Content>
+        <Footer style={{ backgroundColor: '#03152a' }}>Footer</Footer>
+      </Layout>
     </div>
   );
 }
